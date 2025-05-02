7dce3ddc3a321b8d6a333653b9f55e35e7cb3c2e
BayuDC <dev.bayudc@gmail.com>
2025-05-03 04:27:26 +0700

    chore: setup nuxt project

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script>
+import 'animate.css';
+</script>
+
+<template>
+  <Html>
+    <Head>
+      <Title>Edurate</Title>
+    </Head>
+    <Body>
+      <NuxtPage />
+    </Body>
+  </Html>
+</template>
---
+++ components/Animate.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string;
+  infinite?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="animate__animated" :class="['animate__' + name, infinite ? 'animate__infinite' : '']">
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
